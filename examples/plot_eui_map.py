4b015f15e8079df8dfd743aaa3b5235afa503916
Laura Hayes <laurahayes@Lauras-MacBook-Pro.local>
2025-03-12 17:34:29 +0000

    fixing example

--- examples/plot_eui_map.py
+++ examples/plot_eui_map.py
@@ -1,36 +1,46 @@
 """
+==================================
 Finding and Plotting Solar Orbiter EUI Data
-===========================================
+==================================
 
 This example demonstrates how to:
 - Search for Solar Orbiter EUI (Extreme Ultraviolet Imager) data
 - Download a sample image
 - Plot it using SunPy
-
 """
 
-import sunpy.map
 import matplotlib.pyplot as plt
+import sunpy.map
 from sunpy.net import Fido, attrs as a
 import sunpy_soar
 
-# Search for Solar Orbiter EUI images in a given time range
-search_results = Fido.search(a.Time("2022-03-01", "2022-03-01 01:00"), 
-				             a.soar.Product("EUI-FSI174-IMAGE"), 
-				             a.Level(2))
+###############################################################################
+# First, we will use `Fido <sunpy.net.fido_factory.UnifiedDownloaderFactory>`__
+# to search for available Solar Orbiter **EUI-FSI174-IMAGE** data in a specific
+# time range.  
+#
+# The search will return metadata about available files.
 
-# Print search results
+search_results = Fido.search(a.Time("2022-03-01", "2022-03-01 01:00"),
+                             a.soar.Product("EUI-FSI174-IMAGE"),
+                             a.Level(2))
+
 print(search_results)
 
-# Fetch the first available file
+###############################################################################
+# Once we have the search results, we can fetch the **first available file**.
+
 downloaded_files = Fido.fetch(search_results[0, 0])
 
-# Load the downloaded EUI image
+###############################################################################
+# Now that we have the file, we can create a `~sunpy.map.Map` to visualize it.
+
 eui_map = sunpy.map.Map(downloaded_files[0])
 
-# Plot the EUI image
-plt.figure(figsize=(8, 6))
-eui_map.plot()
+fig = plt.figure(figsize=(8, 6))
+ax = fig.add_subplot(projection=eui_map)
+eui_map.plot(axes=ax)
 plt.colorbar()
 plt.title("Solar Orbiter EUI Image")
+
 plt.show()
